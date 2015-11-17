57ca5e294322efe2f5a70d2e663a2dc2bd4fa0b8
LADOSSIFPB <ladossifpb@gmail.com>
2015-11-17 14:00:29 -0300

    Splash screen

---
+++ app/src/main/java/com/example/ifpb/android2bim/Splash.java
@@ -0,0 +1,32 @@
+package com.example.ifpb.android2bim;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class Splash extends Activity{
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Timer().schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                finish();
+
+                Intent intent = new Intent();
+                intent.setClass(Splash.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 6000);
+    }
+
+
+}
